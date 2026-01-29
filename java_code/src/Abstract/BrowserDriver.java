897c41978da3adb24d4e0c0c1aceb529521a7fc6
Rohit <rohitdubeybhadohi123@gmail.com>
2026-01-29 11:54:29 -0800

    oops

---
+++ java_code/src/Abstract/BrowserDriver.java
@@ -0,0 +1,11 @@
+package Abstract;
+
+public interface BrowserDriver {
+
+	
+	public void openbrowser(); 
+		
+	public void Closebrowser();
+	
+	public void navigate();
+}
---
+++ java_code/src/Abstract/Runnerclass.java
@@ -0,0 +1,11 @@
+package Abstract;
+
+public class Runnerclass {
+
+	public static void main(String[] args) {
+		BrowserDriver driver=new Chromedriver_1();
+		driver.Closebrowser();
+
+	}
+
+}
