2b5599968981777715e224d72d9242abcf8ccdab
Rohit <rohitdubeybhadohi123@gmail.com>
2026-02-03 13:20:22 -0800

    excel and notepad data

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition{
+public static void main(String[]args){
+
+int a=10;
+int b=20;
+int c=45;
+int d=a+b+c;
+System.out.println(d);
+
+}
+}
+
+
+//h2[@aria-label='Sponsored Ad - Samsung Galaxy M36 5G (Serene Green, 8 GB RAM, 256 GB Storage)| Circle to Search| Google Gemini| Corning Gorilla Glass Victus+| 7.7mm Slim| AI Enhanced 50 MP OIS Triple Camera| Unmatched Nightography']/ancestor::div[@class='puisg-col-inner']//span[@class='a-offscreen']
